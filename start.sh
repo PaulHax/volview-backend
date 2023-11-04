52d697173ab9e9d77f18ae576275e7d392aa2608
Paul Elliott <paul.elliott@kitware.com>
2023-11-04 10:49:08 -0400

    Add startup scripts and sample files

---
+++ start.sh
@@ -0,0 +1,5 @@
+google-chrome "http://127.0.0.1:8080/?names=[foo.dcm]&urls=[http://localhost:9999/1-001.dcm, http://localhost:9999/config.json]"
+google-chrome "http://127.0.0.1:8080/?names=[volview.zip]&urls=[http://localhost:9999/session.volview.zip, http://localhost:9999/configLabelsView.json]"
+
+npm run start
+
